366b07441cfe920130b3619e38f6f192b6cb0d0c
Leo López <leopoldo.misael.lopez.de.alba@gmail.com>
2021-01-21 20:31:27 -0600

    test maps

---
+++ weather-map/js/data.js
@@ -0,0 +1,67 @@
+var data = {
+    "results": [
+        {
+            "id": 1,
+            "latLng": {
+                lat: 20.6534,
+                lng: -105.22533
+            },
+            "name": "Puerto Vallarta",
+            "weatherType": "sunny"
+        },
+        {
+            "id": 2,
+            "latLng": {
+                lat: 20.34174,
+                lng: -102.76523
+            },
+            "name": "Ocotlán",
+            "weatherType": "cloudy"
+        },
+        {
+            "id": 3,
+            "latLng": {
+                lat: 22.11332,
+                lng: -103.26684
+            },
+            "name": "Colotlán",
+            "weatherType": "sunny"
+        },
+        {
+            "id": 4,
+            "latLng": {
+                lat: 20.67464,
+                lng: -103.38414
+            },
+            "name": "AMG",
+            "weatherType": "cloudySun"
+        },
+        {
+            "id": 5,
+            "latLng": {
+                lat: 19.70501,
+                lng: -103.46851
+            },
+            "name": "Ciudad Guzmán",
+            "weatherType": "cloudy"
+        },
+        {
+            "id": 6,
+            "latLng": {
+                lat: 19.77077,
+                lng: -104.36675
+            },
+            "name": "Autlán de Navarro",
+            "weatherType": "cloudySun"
+        },
+        {
+            "id": 7,
+            "latLng": {
+                lat: 21.36349,
+                lng: -101.9291
+            },
+            "name": "Lagos de Moreno",
+            "weatherType": "cloudy"
+        }
+    ]
+}
